70bc4bbca491c57c49f0a97b16b36168c6253e9c
Jeevan Joshi <joshijeevan98@gmail.com>
2023-04-10 10:27:19 +0530

    initial commit

---
+++ PizzaPlaza/PizzaPlazaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaPlazaApp.swift
+//  PizzaPlaza
+//
+//  Created by Jeevan Chandra Joshi on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PizzaPlazaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
